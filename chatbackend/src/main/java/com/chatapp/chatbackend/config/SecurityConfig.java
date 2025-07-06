0d06595a92374e5a13b4445ffb47599c9728f88d
Gourav Singh <gourav.oct@gmail.com>
2025-07-06 23:05:37 +0530

    permit /chat api

--- chatbackend/src/main/java/com/chatapp/chatbackend/config/SecurityConfig.java
+++ chatbackend/src/main/java/com/chatapp/chatbackend/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(req -> req
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/chat/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
